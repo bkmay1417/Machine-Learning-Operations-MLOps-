20e47b308bd4e12633b8c06ed894d04e70ee9777
Michael Martinez <163685041+bkmay1417@users.noreply.github.com>
2024-05-25 02:27:39 -0300

    asdasd

--- main.py
+++ main.py
@@ -5,21 +5,20 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import json
 import pandas as pd
-"""  
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-"""  
+
 app= FastAPI(title='Proyecto Integrador I Hecho por Michael Martinez')
 templates = Jinja2Templates(directory="templates")
-"""   
-df_recom = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/4e23689d2e1a74fddcc9251cee8614c16618cbcf/Dataset/recomendacion.parquet?raw=True')
 
-# Vectorizar los géneros
+df_recom = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/2596484f682598ca027fdb025193a4a04d317166/Dataset/recomendacion3_v1.parquet?raw=True')
+# Preprocesamiento de los géneros para generar la matriz TF-IDF
 vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(df_recom['genres_str'])
-# Calcular la similitud del coseno
-cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)# optimizar
-"""
+tfidf_matrix = vectorizer.fit_transform(df_recom.groupby('item_id')['genres'].apply(lambda x: ' '.join(x)))
+# Cálculo de la similitud del coseno
+
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
 
 @app.get("/", tags=['Página Principal'])
@@ -137,4 +136,24 @@ async def developer_reviews_analysis(desarrolladora= Query(default='Valve')):
 
     
     return(resultado)
+
+@app.get("/Sistema_de_recomendacion")
+async def recomendacion_juego(item_id : float = Query(default=6010.0)):
+    try:
+        # Obtener el índice del juego en el DataFrame
+        idx = df_recom[df_recom['item_id'] == item_id].index[0]
+    except IndexError:
+        # Manejar el caso donde no se encuentra el ID del juego
+        raise HTTPException(status_code=404, detail="Item ID no encontrado.")
+    
+    # Calcular la similitud de coseno entre el juego dado y todos los juegos
+    sim_scores = list(enumerate(cosine_sim[idx]))
+    # Ordenar los juegos por su similitud de coseno
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)[1:6]
+    # Obtener los índices de los juegos recomendados
+    game_indices = [i[0] for i in sim_scores]
+    # Obtener los títulos de los juegos recomendados
+    juegos_recomendados = df_recom['title'].iloc[game_indices].tolist() 
+    return juegos_recomendados
+
 
