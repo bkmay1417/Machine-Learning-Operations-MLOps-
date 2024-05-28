bf5a5ffbea983f05ab1d7cb4bffd0efb0ae0a3d7
Michael Martinez <163685041+bkmay1417@users.noreply.github.com>
2024-05-28 19:35:14 -0300

    Update

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app= FastAPI(title = 'Machine Learning Operations (MLOps)',
 df_recom = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/d5610d0cdf2412a4fdcc6572f39c0268fe51dae9/Dataset/recomendacion.parquet?raw=True')
 df_games = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/82702a42172b2b0f23c1e24c6f9fdb294c52d78e/Dataset/developer.parquet?raw=True')
 user_data = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/14062c66a031d8e736219f75536dfb552372ac48/Dataset/userdata.parquet?raw=True')
-df = pd.read_parquet(r'https://github.com/bkmay1417/Machine-Learning-Operations-MLOps-/blob/d20e2c29b6ce650305e0a57f85e9c074d4be8b3f/Dataset/UserForGenre.parquet?raw=True')
+df = pd.read_parquet(r'Dataset\UserForGenre.parquet')
 
 
 
@@ -130,6 +130,9 @@ async def UserForGenre(genero:str = Query(default='action')):
     # Encontrar el usuario con más horas jugadas para el género dado
     max_user = user_playtime.idxmax()
     
+    # Filtrar filas que tienen el usuario 
+    df_genero = df[df['user_id'] == max_user]
+
     # Utilizar directamente la columna release_year para obtener el año de lanzamiento
     df_genero['year'] = df_genero['release_date'].astype(float)
     
